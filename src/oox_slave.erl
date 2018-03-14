cd4a92b01ebe140e0eab6aa204bbb38910ab0427
zgbjgg <zgbjgg@gmail.com>
2018-03-14 17:53:48 -0600

    allow parse commands for dynamic usage in worker & dataframe

--- src/oox_slave.erl
+++ src/oox_slave.erl
@@ -2,7 +2,8 @@
 
 -export([unique_serial/0,
     set_options/0,
-    set_options/1]).
+    set_options/1,
+    parse_commands/3]).
 
 unique_serial() ->
     {MS, S, US} = erlang:now(),
@@ -15,3 +16,26 @@ set_options(Cookie) when is_atom(Cookie) ->
     set_options(atom_to_list(Cookie));
 set_options(Cookie)                      ->
     "-setcookie " ++ Cookie.
+
+% for now parsing commands is a great helper to set
+% the worker in every command, using a single syntax from
+% oox client.
+parse_commands([], _For, _Value)                 -> [];
+parse_commands(Commands, _For, none)             -> Commands;
+parse_commands([Command | Commands], For, Value) ->
+    Args = proplists:get_value(args, Command, []),
+    F = build_fun(For, Value),
+    NewArgs = lists:map(F, Args),
+    NewCommand = proplists:delete(args, Command) ++ [{args, NewArgs}],
+    [NewCommand] ++ parse_commands(Commands, For, Value).
+
+%% @hidden
+
+build_fun('$worker', Value)    ->
+    fun('$worker') -> Value;
+       (A)         -> A
+    end;
+build_fun('$dataframe', Value) ->
+    fun('$dataframe') -> {_, DataFrame} = Value, DataFrame;
+       (A)            -> A
+    end.
