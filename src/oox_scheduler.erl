5c2ca06bb2ff16355f68f237e4a9c7f12ddc164a
zgbjgg <zgbjgg@gmail.com>
2018-03-25 14:22:51 -0600

    use queue when slave is not ready & job was started before

--- src/oox_scheduler.erl
+++ src/oox_scheduler.erl
@@ -9,7 +9,8 @@
     start_job/2,
     get_jobs/0,
     set_subscriber/1,
-    get_subscriber/0]).
+    get_subscriber/0,
+    get_queue/0]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -20,7 +21,7 @@
     code_change/3]).
 
 % the values can be override during initialization
--record(state, {jobs = [] :: list(),
+-record(state, {jobs = [] :: list(), queue = [] :: list(),
     subscriber = undefined :: undefined | pid()}).
 
 start_link() ->
@@ -44,6 +45,9 @@ get_subscriber() ->
 set_subscriber(Subscriber) ->
     gen_server:call(?MODULE, {set_subscriber, Subscriber}).
 
+get_queue() ->
+    gen_server:call(?MODULE, get_queue).
+
 init([]) ->
     {ok, #state{jobs = [], subscriber = undefined}}.
 
@@ -58,17 +62,22 @@ handle_call({add_job, Host}, _From, State) ->
     ok = gen_server:call(Job, launch),
     {reply, {ok, Job}, State};
 
-handle_call({start_job, Job, Commands}, _From, State=#state{jobs = Jobs}) ->
+handle_call({start_job, Job, Commands}, _From, State=#state{jobs = Jobs, queue = Queue}) ->
     case lists:member(Job, Jobs) of
         true  ->
             ok = gen_server:cast(Job, {exe, Commands}),
             {reply, {ok, working}, State};
         false ->
-            {reply, {error, no_such_job}, State}
+            % maybe this request arrives when slave is not ready,
+            % just add to the queue and if error, out from there!  
+            {reply, {ok, queuing}, State#state{queue = [{Job, Commands} | Queue]}}
     end;
 
 handle_call(get_jobs, _From, State=#state{jobs = Jobs}) ->
     {reply, {ok, Jobs}, State};
+
+handle_call(get_queue, _From, State=#state{queue = Queue}) ->
+    {reply, {ok, Queue}, State};
 
 handle_call({set_subscriber, Subscriber}, _From, State) ->
     {reply, ok, State#state{subscriber = Subscriber}};
@@ -88,11 +97,21 @@ handle_cast({oox, done_job, Job, Results}, State=#state{jobs = Jobs, subscriber 
     Subscriber ! {job, Job, JobStatus},
     {noreply, State#state{jobs = Jobs -- [Job]}};
 
-handle_cast({oox, up, Job}, State=#state{jobs = Jobs}) ->
-    {noreply, State#state{jobs = [Job | Jobs]}};
+handle_cast({oox, up, Job}, State=#state{jobs = Jobs, queue = Queue}) ->
+    % maybe job was already in queue, so execute inmediatly the exec commands
+    NQueue = case lists:keyfind(Job, 1, Queue) of
+        {Job, Commands} ->
+            ok = gen_server:cast(Job, {exe, Commands}),
+            lists:keydelete(Job, 1, Queue); 
+        false           ->
+            Queue
+    end,
+    {noreply, State#state{jobs = [Job | Jobs], queue = NQueue}};
 
-handle_cast({oox, error, _Job, _Error}, State) ->
-    {noreply, State};
+handle_cast({oox, error, Job, _Error}, State=#state{queue = Queue}) ->
+    % remove from queue if any
+    NQueue = lists:keydelete(Job, 1, Queue),
+    {noreply, State#state{queue = NQueue}};
 
 handle_cast(_Request, State) ->
     {noreply, State}.
